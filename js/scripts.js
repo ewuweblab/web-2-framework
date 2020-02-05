c26014735a2067ed7bcb25f05fda60f0505d1b2e
Erik Stewart-Smith <59583004+erik1968@users.noreply.github.com>
2020-02-05 11:36:39 -0800

    Updated JS with JS from Current CodePen

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
-console.log('OK');
 // Initialize player
-const player = new Plyr('#player', {
+const player = new Plyr( '#player', {
+  
+  // Turn on/off conrols  
+  controls : [ 'play', 'current-time', 'progress' ],
+  
+  // Turn    
+  keyboard : { focused: true, global: true },
+  
+  seekTime : 15
 
-    controls : [ 'play', 'current-time', 'progress' ]
 });
