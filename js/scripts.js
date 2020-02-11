367e75027deb5549e8df10a3b85b230fa3ac2140
erik1968 <59583004+erik1968@users.noreply.github.com>
2020-02-10 22:26:45 -0800

    Re-Copy/Pasted plyr.min.js & scripts.js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,13 @@
 // Initialize player
-console.log('OK');
-
-// Initialize player
 const player = new Plyr( '#player', {
   
-  // Turn on/off conrols  
+  // Turn on/off conrols. Array data type
   controls : [ 'play', 'current-time', 'progress' ],
   
-  // Turn    
+  // Turn on keyboard. Boolean data type
   keyboard : { focused: true, global: true },
   
+  // Increase/Decrease seek time. Integer data type
   seekTime : 15
 
 });
-
-
